ebc0a21161a23244ea7eacee6038b22d35255dbb
stevenschmutz <steven@prexence.co.za>
2024-01-24 15:30:43 +0200

    Adding volume controls on NAV layer - cheapino

--- keyboards/cheapino/keymaps/stevenschmutz/keymap.c
+++ keyboards/cheapino/keymaps/stevenschmutz/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_PGDN, KC_UP, KC_PGUP, KC_END,
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
- KC_NO, KC_NO, KC_NO, KC_F2, CTRL_TICK,                                     KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
+ KC_MUTE, KC_VOLD, KC_VOLU, KC_F2, CTRL_TICK,                                     KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,                                     LCTL(KC_UP), LCTL(KC_DOWN), KC_NO, KC_NO, KC_TRNS,
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
