d552e51544f37b6b7a7c0065a7a6bbc6271ca77f
stevenschmutz <steven@prexence.co.za>
2024-03-16 08:01:57 +0200

    Initial working version of qmk for macropad_webbes

---
+++ keyboards/handwired/autviam/macropad_webbes/keymaps/default/keymap.c
@@ -0,0 +1,23 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ A │   │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_A,             KC_C,
+        KC_D,    KC_E,    KC_F
+    )
+};
+
+
+
