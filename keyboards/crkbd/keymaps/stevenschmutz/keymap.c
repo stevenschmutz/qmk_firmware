8529551043fcb35bed8b652c54b2a27c452d0efb
stevenschmutz <steven@prexence.co.za>
2024-04-23 09:33:44 +0200

    Removed artsey from combos

--- keyboards/crkbd/keymaps/stevenschmutz/keymap.c
+++ keyboards/crkbd/keymaps/stevenschmutz/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_NO, LSFT_T(KC_SCLN), KC_Q, KC_J, KC_K, KC_X,                             KC_B, KC_M, KC_W, KC_V, RSFT_T(KC_Z), KC_NO,
               //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-                        LCTL_T(KC_ESC), LSFT_T(KC_TAB), LT(2,KC_SPC),                     SC_SENT, LT(1,KC_BSPC), LALT_T(KC_DEL)
+                        KC_TRNS, LSFT_T(KC_TAB), LT(2,KC_SPC),                     SC_SENT, LT(1,KC_BSPC), KC_TRNS
 
             ),
 
