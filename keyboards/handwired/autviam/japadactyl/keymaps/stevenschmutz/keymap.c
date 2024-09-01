1b4eea6ccee1fd6cee8d4d6a1cdc6a293190837d
stevenschmutz <steven@prexence.co.za>
2024-09-01 11:24:54 +0200

    Re-add thumb cluster

--- keyboards/handwired/autviam/japadactyl/keymaps/stevenschmutz/keymap.c
+++ keyboards/handwired/autviam/japadactyl/keymaps/stevenschmutz/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       KC_SCLN, KC_Q, KC_J, KC_K, CKC_X,                             KC_B, KC_M, KC_W, KC_V, RSFT_T(KC_Z),
       //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-                  LSFT_T(KC_TAB), LT(2,KC_SPC), KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
+                  LSFT_T(KC_TAB), LT(2,KC_SPC), KC_TRNS,                   KC_TRNS, SC_SENT, LT(1,KC_BSPC)
 
                 ),
         [_SYMBOL] = LAYOUT_split_3x5_3(
