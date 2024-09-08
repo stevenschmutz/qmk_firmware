b99ec57a452ddbba1e08e4a048b98e57d56e1582
stevenschmutz <steven@prexence.co.za>
2024-09-08 16:28:21 +0200

    Initial copy of japadactyl for ScottoFly - no testing

---
+++ keyboards/handwired/autviam/ScottoFly/rules.mk
@@ -0,0 +1,9 @@
+CLTO_ENABLE = yes
+COMBO_ENABLE = yes
+APS_WORD_ENABLE = yes
+CUSTOM_MATRIX = lite
+WS2812_DRIVER = vendor
+RGBLIGHT_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
+SRC += encoder.c
+SRC += matrix.c
