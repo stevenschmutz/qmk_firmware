304ffd4ed6955caeddbb378b19b35e548793a69f
stevenschmutz <steven@prexence.co.za>
2025-03-19 10:14:01 +0200

    Not working - but used to increase the layers and combos for smol

--- keyboards/handwired/autviam/smol/config.h
+++ keyboards/handwired/autviam/smol/config.h
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define VIAL_COMBO_ENTRIES 40
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_PIN_SWAP
 #define SERIAL_USART_TX_PIN GP0
