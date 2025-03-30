9ad441442efc5692782261d624075e8db80298a7
stevenschmutz <steven@prexence.co.za>
2025-03-30 12:39:02 +0200

    Non-working QMK for SMOL

--- keyboards/handwired/autviam/smolcrkbd/rules.mk
+++
@@ -1,9 +0,0 @@
-# Build Options
-#   change yes to no to disable
-#
-DEFAULT_FOLDER = crkbd/rev1
-DEFERRED_EXEC_ENABLE = yes
-LTO_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-EXTRAKEY_ENABLE = no
