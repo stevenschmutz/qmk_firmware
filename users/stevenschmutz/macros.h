2f0a56a4a35d54b7edfe31fcd90811548b1701be
stevenschmutz <steven@prexence.co.za>
2025-09-25 22:02:00 +0200

    Add backspace and delete in one key

--- users/stevenschmutz/macros.h
+++ users/stevenschmutz/macros.h
@@ -63,6 +63,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return false;
   break;
 
+    case KC_BSPC: {
+      static uint16_t registered_key = KC_NO;
+      if (record->event.pressed) {  // On key press.
+        const uint8_t mods = get_mods();
+#ifndef NO_ACTION_ONESHOT
+        uint8_t shift_mods = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
+#else
+        uint8_t shift_mods = mods & MOD_MASK_SHIFT;
+#endif  // NO_ACTION_ONESHOT
+        if (shift_mods) {  // At least one shift key is held.
+          registered_key = KC_DEL;
+          // If one shift is held, clear it from the mods. But if both
+          // shifts are held, leave as is to send Shift + Del.
+          if (shift_mods != MOD_MASK_SHIFT) {
+#ifndef NO_ACTION_ONESHOT
+            del_oneshot_mods(MOD_MASK_SHIFT);
+#endif  // NO_ACTION_ONESHOT
+            unregister_mods(MOD_MASK_SHIFT);
+          }
+        } else {
+          registered_key = KC_BSPC;
+        }
+
+        register_code(registered_key);
+        set_mods(mods);
+      } else {  // On key release.
+        unregister_code(registered_key);
+      }
+    } return false;
+
+
+
+
     case PEE_PASTE:  // Comma on tap, Ctrl+C on long press.
       return process_tap_or_long_press_key(record, C(KC_V));
       break;
