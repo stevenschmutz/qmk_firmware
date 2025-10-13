d24e735c4894c4992acec38b43155df6ca19b343
stevenschmutz <steven@prexence.co.za>
2025-10-13 10:17:07 +0200

    Fix the k and quot

--- keyboards/handwired/autviam/japadactyl/keymaps/stevenschmutz/keymap.c
+++ keyboards/handwired/autviam/japadactyl/keymaps/stevenschmutz/keymap.c
@@ -53,11 +53,11 @@ CW_TOGG <= capitilise word with _
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT_split_3x5_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_QUOT, COMMA_CUT, DOT_COPY, PEE_PASTE, LT(_NAV,KC_Y),                         KC_F, KC_G, KC_C, KC_R, KC_L,
+        LT(_ART_BASE,KC_QUOT), COMMA_CUT, DOT_COPY, PEE_PASTE, LT(_NAV,KC_Y),                         KC_F, KC_G, KC_C, KC_R, KC_L,
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        CKC_A, CKC_O, CKC_E , CKC_U, LT(_NUMERIC,KC_I),                                          CKC_D , CKC_H , KC_T , CKC_N , KC_S,
 
-      CKC_SCLN, CKC_Q, CKC_J, LT(_ART_BASE,KC_K), LT(_TAIPO,KC_X),                             KC_B, CKC_M, KC_W, KC_V, ALT_DOWN,
+      CKC_SCLN, CKC_Q, CKC_J, CKC_K, LT(_TAIPO,KC_X),                             KC_B, CKC_M, KC_W, KC_V, ALT_DOWN,
       //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                  KC_TAB, LT(_NAV,KC_SPC), KC_NO,                   KC_NO, SC_SENT, LT(_NUMERIC,CW_TOGG)
 
