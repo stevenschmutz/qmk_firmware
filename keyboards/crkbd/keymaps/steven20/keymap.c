37769c793b6ae37892987906df65a0199cd6cab9
stevenschmutz <steven@prexence.co.za>
2026-08-12 20:41:46 +0200

    Updated with tapdance for g.c.r,l

--- keyboards/crkbd/keymaps/steven20/keymap.c
+++ keyboards/crkbd/keymaps/steven20/keymap.c
@@ -154,6 +154,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+
+//Tap Dance Declarations
+enum {
+    TD_ESC_CAPS = 0,
+    TD_CEE_TEE = 0,
+    TD_GEE_AITCH =0,
+    TD_ARR_EN =0,
+    TD_EL_ES =0,
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+  [TD_CEE_TEE]  = ACTION_TAP_DANCE_DOUBLE(KC_C, KC_T),
+  [TD_GEE_AITCH]  = ACTION_TAP_DANCE_DOUBLE(KC_G, KC_H),
+  [TD_ARR_EN]  = ACTION_TAP_DANCE_DOUBLE(KC_R, KC_N),
+  [TD_EL_ES]  = ACTION_TAP_DANCE_DOUBLE(KC_L, KC_S),
+
+// Other declarations would go here, separated by commas, if you have them
+};
+
+
 void housekeeping_task_user(void) {
   layer_lock_task();
   // Other tasks ...
