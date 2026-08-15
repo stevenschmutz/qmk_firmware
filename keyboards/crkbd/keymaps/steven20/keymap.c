1ea5ed08713c2000249a059b05c4d185d30bf9e0
stevenschmutz <steven@prexence.co.za>
2026-08-15 17:40:42 +0200

    Added tap-doubletap-hold

--- keyboards/crkbd/keymaps/steven20/keymap.c
+++ keyboards/crkbd/keymaps/steven20/keymap.c
@@ -24,6 +24,7 @@ enum {
     TD_GEE_AITCH,
     TD_ARR_EN,
     TD_EL_ES,
+   TD_ESC_LAYER,
 };
 
 
@@ -33,7 +34,20 @@ enum {
 #include "features/layer_lock.h"
 #include "macros.h"
 
+// Single tap = ESC, Double tap = CAPS, Hold = Layer 1
+void esc_layer_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed) {
+        layer_on(1);  // Held down -> activate layer 1
+    } else if (state->count == 1) {
+        tap_code(KC_ESC);
+    } else if (state->count >= 2) {
+        tap_code(KC_CAPS);
+    }
+}
 
+void esc_layer_reset(tap_dance_state_t *state, void *user_data) {
+    layer_off(1);  // Release the hold -> turn layer 1 back off
+}
 
 // Tap dance actions
 tap_dance_action_t tap_dance_actions[] = {
@@ -41,6 +55,11 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_GEE_AITCH] = ACTION_TAP_DANCE_DOUBLE( KC_H,KC_G),
     [TD_ARR_EN]  = ACTION_TAP_DANCE_DOUBLE(KC_N,KC_R),
     [TD_EL_ES]  = ACTION_TAP_DANCE_DOUBLE(KC_S, KC_L),
+    [TD_ESC_LAYER] = ACTION_TAP_DANCE_FN_ADVANCED(
+        NULL,
+        esc_layer_finished,
+        esc_layer_reset
+    ),
 };
 
 
